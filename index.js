0ca389fdf188fc905a70f362300237a8c807108d
elianluciongm <108909831+elianluciongm@users.noreply.github.com>
2025-04-01 21:10:00 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
 import express from "express";
+import { Sequelize } from "sequelize";
+
+const sequelize = new Sequelize('biblioteca2025', 'postgres', '20052003', {
+    host: 'localhost',
+    port: 5432,
+    dialect:'postgres',
+    define: {
+        timestamps: false,
+        freezeTableName: true
+    }
+});
+
+try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
 
 const app = express();
 app.use(express.json());
@@ -10,3 +28,4 @@ app.get('/teste', (req, res) => {
 app.listen(3000, () => { 
     console.log("Servidor rodando.");
 });
+
