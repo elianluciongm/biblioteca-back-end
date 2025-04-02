deb0e4cc698c0467329855fe2a1aeb53199207b6
elianluciongm <108909831+elianluciongm@users.noreply.github.com>
2025-04-01 21:31:45 -0300

    Mapeamento da model editora

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
-import { Sequelize } from "sequelize";
+import { Sequelize, DataTypes } from "sequelize";
 
+//Conexão com o banco de dados
 const sequelize = new Sequelize('biblioteca2025', 'postgres', '20052003', {
     host: 'localhost',
     port: 5432,
@@ -11,11 +12,39 @@ const sequelize = new Sequelize('biblioteca2025', 'postgres', '20052003', {
     }
 });
 
+//Mapeamento da model editora
+const Editora = sequelize.define(
+    'editora',
+    {
+      ideditora: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+      },
+
+      nomeeditora: {
+      type: DataTypes.STRING(60),
+      allowNull: false
+      },
+
+      cnpj: {
+      type: DataTypes.STRING,
+      allowNull: false
+      },
+
+      endereco: {
+        type: DataTypes.TEXT,
+        allowNull: true
+      }
+    }
+  );
+
 try {
     await sequelize.authenticate();
-    console.log('Connection has been established successfully.');
+    console.log('Conexão com o banco de dados realizada com sucesso');
   } catch (error) {
-    console.error('Unable to connect to the database:', error);
+    console.error('Erro ao conectar ao banco de dados', error);
   }
 
 const app = express();
