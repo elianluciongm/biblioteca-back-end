7b93510b771640035187b716975189f27d243cec
elianluciongm <108909831+elianluciongm@users.noreply.github.com>
2025-04-08 19:53:59 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -54,7 +54,48 @@ app.get('/teste', (req, res) => {
     res.send('Teste ok');
 });
 
-app.listen(3000, () => { 
-    console.log("Servidor rodando.");
+//Rotas Crud da tabela editora
+app.get('/editora', async (req, res) => {
+    const respostaBanco = await Editora.findAll();
+    res.json(respostaBanco);
 });
+
+
+app.get('/editora/:id', async (req, res)=>{
+    const id = req.params.id
+    const respostaBanco = await Editora.findByPk(id);
+    res.json(respostaBanco);
+});
+
+app.post('/editora', async (req, res)=>{
+    const nomeeditora = req.body.nomeeditora;
+    const cnpj = req.body.cnpj;
+    const endereco = req.body.endereco;
+    const respostaBanco = await Editora.create(req.body);
+    res.json(respostaBanco);
+});
+
+app.put('/editora/:id', async (req, res) => {
+  const nomeeditora = req.body.nomeeditora;
+  const cnpj = req.body.cnpj;
+  const endereco = req.body.endereco;
+  const ideditora = req.params.id;
+
+  const respostaBanco = await Editora.update(
+      { nomeeditora, cnpj, endereco },
+      { where: { ideditora } }
+  );
+  res.json(respostaBanco);
+});
+
+app.delete('/editora/:id', async (req, res) => {
+  const ideditora = req.params.id;
+  const respostaBanco = await Editora.destroy({ where: { ideditora } });
+  res.json(respostaBanco);
+});
+
+
+app.listen(2000, () => { console.log("Servidor rodando.")});
+
+
 
